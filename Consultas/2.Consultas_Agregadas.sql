415290289a262b2da8d2e8e29c0abca3daa885c7
Quilez42 <quilezruizraul@gmail.com>
2025-03-06 17:42:21 +0100

    Update 2.Consultas_Agregadas.sql

--- Consultas/2.Consultas_Agregadas.sql
+++ Consultas/2.Consultas_Agregadas.sql
@@ -1,13 +1,13 @@
 -- CONSULTAS AGREGADAS
 
---Los telefonos y dni de los clientes que tienen gatos
-SELECT t.dniCliente, t.telefono
+--Los teléfonos y dni de los clientes que tienen gatos.
+SELECT DISTINCT(t.dniCliente, t.telefono)
 from telefono t 
 join cliente c on t.dniCliente=c.dni
 join animal a on c.dni=a.dni_cliente
 WHERE a.especie LIKE 'Gato';
 
---Todos los clientes que han tenido una cita el 25 de diciembre de 2024
+--Todos los clientes que han tenido una cita el 25 de diciembre de 2024.
 SELECT c.dni 
 from cliente c 
 JOIN animal a ON c.dni=a.dni_cliente 
@@ -15,7 +15,7 @@ JOIN historial h ON a.id=h.id_animal
 JOIN cita ci ON h.id_cita=ci.id 
 WHERE ci.fecha='2024-12-25';
 
---Dinero  invertido en sueldos por cada centro
+--Dinero  invertido en sueldos por cada centro.
 SELECT t.codCentro, SUM(e.sueldo)
 from trabajar t 
 join empleado e on t.dniempleado=e.dni 
@@ -29,7 +29,7 @@ JOIN cita c on r.dni=c.dniRecepcionista
 GROUP BY r.dni 
 HAVING CitasConcedidas>10;
 
---Animal o animales que han tenido mas cirujías 
+--Animal o animales que han tenido más cirujías 
 select h.* 
   from historial h join tratamiento t on h.id_tratamiento = t.id 
   where dni_cirujano is not null;
@@ -40,7 +40,7 @@ FROM HISTORIAL
 JOIN ANIMAL ON HISTORIAL.id_animal = ANIMAL.id
 JOIN TRATAMIENTO ON HISTORIAL.id_tratamiento = TRATAMIENTO.id;
 
--- Devuelve los centros que no ha tenido citas en los ultimos 7 dias.
+-- Devuelve los centros que no ha tenido citas en los ultimos 7 días.
 select c.cod from centro c
 join sala s on c.cod=s.cod_centro
 LEFT join cita ci on s.nombre=ci.nombre_sala
@@ -65,7 +65,7 @@ WHERE t.fecha >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
 GROUP BY v.dni, e.nombre
 HAVING COUNT(t.id) > 5;
 
--- Nombre de los cirujanos y la cantidad de citas que han tenido en los ultimos 6 meses.
+-- Nombre de los cirujanos y la cantidad de citas que han tenido en los últimos 6 meses.
 
 SELECT c.dni, e.nombre, COUNT(ci.id) AS total_citas
 FROM cirujano c
@@ -76,14 +76,14 @@ JOIN cita ci ON h.id_cita = ci.id
 WHERE ci.fecha >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
 GROUP BY c.dni, e.nombre;
 
---Los animales que mas tratamientos han tenido
+--Los animales que mas tratamientos han tenido.
 SELECT a.id, a.nombre, COUNT(h.id_tratamiento),
 RANK() OVER (ORDER BY COUNT(h.id_tratamiento) DESC) as ranking_tratamientos
 FROM historial h 
 JOIN animal a ON h.id_animal = a.id
 GROUP BY a.id;
 
---Información de las citas
+--Información de las citas.
 CREATE VIEW vista_citas AS (
 SELECT c.id, c.nombre_sala, c.fecha, c.hora, e.nombre
 FROM cita c
@@ -91,7 +91,7 @@ JOIN recepcionista r ON c.dniRecepcionista = r.dni
 JOIN empleado e ON r.dni = e.dni
 );
 
---Datos de los clientess y sus animales
+--Datos de los clientes y sus animales.
 CREATE VIEW vista_clientes_mascotas AS (
 SELECT c.dni, c.nombre, a.nombre, a.especie, a.raza, a.edad
 FROM cliente c
